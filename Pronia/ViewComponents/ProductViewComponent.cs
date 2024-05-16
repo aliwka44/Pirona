9481b0340a3e0b494ab381fea4994f25931c9c59
aliwka44 <sukurovali17@gmail.com>
2024-05-16 06:52:53 +0400

    code

---
+++ Pronia/ViewComponents/ProductViewComponent.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Pronia.DataAccesLayer;
+using Pronia.Models;
+
+namespace Pronia.ViewComponents
+{
+    public class ProductViewComponent:ViewComponent
+    {
+        private readonly ProniaContext _context;
+
+        public ProductViewComponent(ProniaContext context)
+        {
+            _context = context;
+        }
+        public async Task<IViewComponentResult> InvokeAsync(int order=1)
+        {
+
+
+            List<Product> products=null;
+
+
+            switch (order)
+            {
+                case 1:
+                    products = await _context.Products.OrderBy(p => p.Name).Take(8).ToListAsync();
+                    break;
+                case 2:
+                    products = await _context.Products.OrderByDescending(p => p.SellPrice).Take(8).ToListAsync();
+                    break;
+                case 3:
+                    products = await _context.Products.OrderBy(p => p.CreatedTime).Take(8).ToListAsync();
+                    break;
+
+
+            }
+
+            return View(products);
+
+            //return View(Task.FromResult(products));
+        }
+    }
+}
